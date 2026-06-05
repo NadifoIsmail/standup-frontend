cdb04d0e0889c5a0e639bca6833bf1a12f1d28d1
Nadifo Ismail <nadifoismail80@gmail.com>
2026-06-05 03:22:57 +0300

    Made some changes in services

--- src/services/standupService.js
+++ src/services/standupService.js
@@ -1,28 +1,12 @@
-// import api from "./api";
-
-// // GET all standups
-// export const getStandups = () => {
-//   return api.get("/standups/");
-// };
-
-// // POST new standup
-// export const createStandup = (data) => {
-//   return api.post("/standups/", data);
-// };
-
-// // GET stats
-// export const getStandupStats = () => {
-//   return api.get("/standups/stats/");
-// };
 
 import api from "./api";
 
 export const getStandups = () => {
-  return api.get("/standups"); 
+  return api.get("/standups/"); 
 };
 
 export const createStandup = (data) => {
-  return api.post("/standups", data, {  
+  return api.post("/standups/", data, {  
     headers: {
       "Content-Type": "multipart/form-data",
     },
@@ -30,5 +14,5 @@ export const createStandup = (data) => {
 };
 
 export const getStandupStats = () => {
-  return api.get("/standups/stats");
+  return api.get("/standups/stats/");
 };
